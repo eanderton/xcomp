55ee441129d985404e1ec9fcf38e0d58d2887e93
eanderton <eric.t.anderton@gmail.com>
2020-10-14 20:18:19 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools
+import subprocess
+from setuptools.command.develop import develop
+from xcomp.version import __VERSION__
+
+# shim to install dev depenedencies on 'setup.py develop'
+class ExtDevelop(develop):
+    def install_for_development(self):
+        from distutils import log
+        develop.install_for_development(self)
+        if 'develop' in self.distribution.extras_require:
+            log.info('\nInstalling development dependencies')
+            requirements = ' '.join(self.distribution.extras_require['develop'])
+            proc = subprocess.Popen('pip install ' + requirements, shell=True)
+            proc.wait()
+
+
+setuptools.setup(
+     name='xcomp',
+     version=__VERSION__,
+     description='XComp 6502 assembler',
+     long_description=open('README.md').read().strip(),
+     author='Eric Anderton',
+     author_email='eric.t.anderton@gmail.com',
+     url='http://github.com/eanderton/xcomp',
+     packages=['xcomp'],
+     test_suite='tests',
+     install_requires=[],
+     extras_require={
+        'develop': ['coverage'],
+     },
+     cmdclass= {
+        'develop': ExtDevelop,
+     },
+     license='MIT License',
+     zip_safe=False,
+     keywords='compiler compilers 6502 6510',
+     classifiers=[
+         'Packages'
+         'Development Status :: 4 - Beta',
+         'Intended Audience :: Developers',
+         'License :: OSI Approved :: MIT License',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.6',
+     ])
