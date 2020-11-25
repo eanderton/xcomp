7e49a1b1a1da8f8595ad6ff301a42abfe63169ea
eanderton <eric.t.anderton@gmail.com>
2020-11-25 16:42:44 -0500

    moved generated parser into parser specification

--- xcomp/parser.py
+++ xcomp/parser.py
@@ -96,6 +96,218 @@ a_tok           = "a"
 x_tok           = "x"
 y_tok           = "y"
 
+adc_tok = "adc"
+and_tok = "and"
+asl_tok = "asl"
+bcc_tok = "bcc"
+bcs_tok = "bcs"
+beq_tok = "beq"
+bit_tok = "bit"
+bmi_tok = "bmi"
+bne_tok = "bne"
+bpl_tok = "bpl"
+brk_tok = "brk"
+bvc_tok = "bvc"
+clc_tok = "clc"
+cld_tok = "cld"
+cli_tok = "cli"
+clv_tok = "clv"
+cmp_tok = "cmp"
+cpx_tok = "cpx"
+cpy_tok = "cpy"
+dec_tok = "dec"
+eor_tok = "eor"
+inc_tok = "inc"
+inx_tok = "inx"
+iny_tok = "iny"
+jmp_tok = "jmp"
+jsr_tok = "jsr"
+lda_tok = "lda"
+ldx_tok = "ldx"
+ldy_tok = "ldy"
+lsr_tok = "lsr"
+nop_tok = "nop"
+ora_tok = "ora"
+pha_tok = "pha"
+php_tok = "php"
+pla_tok = "pla"
+plp_tok = "plp"
+rol_tok = "rol"
+ror_tok = "ror"
+rti_tok = "rti"
+rts_tok = "rts"
+sbc_tok = "sbc"
+sec_tok = "sec"
+sed_tok = "sed"
+sei_tok = "sei"
+sta_tok = "sta"
+stx_tok = "stx"
+sty_tok = "sty"
+tax_tok = "tax"
+tay_tok = "tay"
+tsx_tok = "tsx"
+txa_tok = "txa"
+txs_tok = "txs"
+tya_tok = "tya"
+
+op_adc_immediate = adc_tok _ hash_tok _ expr
+op_adc_zeropage = adc_tok _ expr
+op_adc_zeropage_x = adc_tok _ expr _ comma_tok _ x_tok
+op_adc_absolute = adc_tok _ expr
+op_adc_absolute_x = adc_tok _ expr16 _ comma_tok _ x_tok
+op_adc_absolute_y = adc_tok _ expr16 _ comma_tok _ y_tok
+op_adc_indirect_x = adc_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_adc_indirect_y = adc_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_and_immediate = and_tok _ hash_tok _ expr
+op_and_zeropage = and_tok _ expr
+op_and_zeropage_x = and_tok _ expr _ comma_tok _ x_tok
+op_and_absolute = and_tok _ expr
+op_and_absolute_x = and_tok _ expr16 _ comma_tok _ x_tok
+op_and_absolute_y = and_tok _ expr16 _ comma_tok _ y_tok
+op_and_indirect_x = and_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_and_indirect_y = and_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_asl_accumulator = asl_tok _ a_tok
+op_asl_zeropage = asl_tok _ expr
+op_asl_zeropage_x = asl_tok _ expr _ comma_tok _ x_tok
+op_asl_absolute = asl_tok _ expr
+op_asl_absolute_x = asl_tok _ expr16 _ comma_tok _ x_tok
+op_bcc_relative = bcc_tok _ expr
+op_bcs_relative = bcs_tok _ expr
+op_beq_relative = beq_tok _ expr
+op_bit_zeropage = bit_tok _ expr
+op_bit_absolute = bit_tok _ expr
+op_bmi_relative = bmi_tok _ expr
+op_bne_relative = bne_tok _ expr
+op_bpl_relative = bpl_tok _ expr
+op_brk_implied = _ brk_tok
+op_bvc_relative = bvc_tok _ expr
+op_bvc_relative = bvc_tok _ expr
+op_clc_implied = _ clc_tok
+op_cld_implied = _ cld_tok
+op_cli_implied = _ cli_tok
+op_clv_implied = _ clv_tok
+op_cmp_immediate = cmp_tok _ hash_tok _ expr
+op_cmp_zeropage = cmp_tok _ expr
+op_cmp_zeropage_x = cmp_tok _ expr _ comma_tok _ x_tok
+op_cmp_absolute = cmp_tok _ expr
+op_cmp_absolute_x = cmp_tok _ expr16 _ comma_tok _ x_tok
+op_cmp_absolute_y = cmp_tok _ expr16 _ comma_tok _ y_tok
+op_cmp_indirect_x = cmp_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_cmp_indirect_y = cmp_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_cpx_immediate = cpx_tok _ hash_tok _ expr
+op_cpx_zeropage = cpx_tok _ expr
+op_cpx_absolute = cpx_tok _ expr
+op_cpy_immediate = cpy_tok _ hash_tok _ expr
+op_cpy_zeropage = cpy_tok _ expr
+op_cpy_absolute = cpy_tok _ expr
+op_dec_zeropage = dec_tok _ expr
+op_dec_zeropage_x = dec_tok _ expr _ comma_tok _ x_tok
+op_dec_absolute = dec_tok _ expr
+op_dec_absolute_x = dec_tok _ expr16 _ comma_tok _ x_tok
+op_dec_implied = _ dec_tok
+op_dec_implied = _ dec_tok
+op_eor_immediate = eor_tok _ hash_tok _ expr
+op_eor_zeropage = eor_tok _ expr
+op_eor_zeropage_x = eor_tok _ expr _ comma_tok _ x_tok
+op_eor_absolute = eor_tok _ expr
+op_eor_absolute_x = eor_tok _ expr16 _ comma_tok _ x_tok
+op_eor_absolute_y = eor_tok _ expr16 _ comma_tok _ y_tok
+op_eor_indirect_x = eor_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_eor_indirect_y = eor_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_inc_zeropage = inc_tok _ expr
+op_inc_zeropage_x = inc_tok _ expr _ comma_tok _ x_tok
+op_inc_absolute = inc_tok _ expr
+op_inc_absolute_x = inc_tok _ expr16 _ comma_tok _ x_tok
+op_inx_implied = _ inx_tok
+op_iny_implied = _ iny_tok
+op_jmp_absolute = jmp_tok _ expr
+op_jmp_indirect = jmp_tok _ lparen_tok _ expr _ rparen_tok
+op_jsr_absolute = jsr_tok _ expr
+op_lda_immediate = lda_tok _ hash_tok _ expr
+op_lda_zeropage = lda_tok _ expr
+op_lda_zeropage_x = lda_tok _ expr _ comma_tok _ x_tok
+op_lda_absolute = lda_tok _ expr
+op_lda_absolute_x = lda_tok _ expr16 _ comma_tok _ x_tok
+op_lda_absolute_y = lda_tok _ expr16 _ comma_tok _ y_tok
+op_lda_indirect_x = lda_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_lda_indirect_y = lda_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_ldx_immediate = ldx_tok _ hash_tok _ expr
+op_ldx_zeropage = ldx_tok _ expr
+op_ldx_zeropage_y = ldx_tok _ expr _ comma_tok _ y_tok
+op_ldx_absolute = ldx_tok _ expr
+op_ldx_absolute_y = ldx_tok _ expr16 _ comma_tok _ y_tok
+op_ldy_immediate = ldy_tok _ hash_tok _ expr
+op_ldy_zeropage = ldy_tok _ expr
+op_ldy_zeropage_x = ldy_tok _ expr _ comma_tok _ x_tok
+op_ldy_absolute = ldy_tok _ expr
+op_ldy_absolute_x = ldy_tok _ expr16 _ comma_tok _ x_tok
+op_lsr_accumulator = lsr_tok _ a_tok
+op_lsr_zeropage = lsr_tok _ expr
+op_lsr_zeropage_x = lsr_tok _ expr _ comma_tok _ x_tok
+op_lsr_absolute = lsr_tok _ expr
+op_lsr_absolute_x = lsr_tok _ expr16 _ comma_tok _ x_tok
+op_nop_implied = _ nop_tok
+op_ora_immediate = ora_tok _ hash_tok _ expr
+op_ora_zeropage = ora_tok _ expr
+op_ora_zeropage_x = ora_tok _ expr _ comma_tok _ x_tok
+op_ora_absolute = ora_tok _ expr
+op_ora_absolute_x = ora_tok _ expr16 _ comma_tok _ x_tok
+op_ora_absolute_y = ora_tok _ expr16 _ comma_tok _ y_tok
+op_ora_indirect_x = ora_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_ora_indirect_y = ora_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_pha_implied = _ pha_tok
+op_php_implied = _ php_tok
+op_pla_implied = _ pla_tok
+op_plp_implied = _ plp_tok
+op_rol_accumulator = rol_tok _ a_tok
+op_rol_zeropage = rol_tok _ expr
+op_rol_zeropage_x = rol_tok _ expr _ comma_tok _ x_tok
+op_rol_absolute = rol_tok _ expr
+op_rol_absolute_x = rol_tok _ expr16 _ comma_tok _ x_tok
+op_ror_accumulator = ror_tok _ a_tok
+op_ror_zeropage = ror_tok _ expr
+op_ror_zeropage_x = ror_tok _ expr _ comma_tok _ x_tok
+op_ror_absolute = ror_tok _ expr
+op_ror_absolute_x = ror_tok _ expr16 _ comma_tok _ x_tok
+op_rti_implied = _ rti_tok
+op_rts_implied = _ rts_tok
+op_sbc_immediate = sbc_tok _ hash_tok _ expr
+op_sbc_zeropage = sbc_tok _ expr
+op_sbc_zeropage_x = sbc_tok _ expr _ comma_tok _ x_tok
+op_sbc_absolute = sbc_tok _ expr
+op_sbc_absolute_x = sbc_tok _ expr16 _ comma_tok _ x_tok
+op_sbc_absolute_y = sbc_tok _ expr16 _ comma_tok _ y_tok
+op_sbc_indirect_x = sbc_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_sbc_indirect_y = sbc_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_sec_implied = _ sec_tok
+op_sed_implied = _ sed_tok
+op_sei_implied = _ sei_tok
+op_sta_zeropage = sta_tok _ expr
+op_sta_zeropage_x = sta_tok _ expr _ comma_tok _ x_tok
+op_sta_absolute = sta_tok _ expr
+op_sta_absolute_x = sta_tok _ expr16 _ comma_tok _ x_tok
+op_sta_absolute_y = sta_tok _ expr16 _ comma_tok _ y_tok
+op_sta_indirect_x = sta_tok _ lparen_tok _ expr _ comma_tok _ x_tok _ rparen_tok
+op_sta_indirect_y = sta_tok _ lparen_tok _ expr _ rparen_tok _ comma_tok _ y_tok
+op_stx_zeropage = stx_tok _ expr
+op_stx_zeropage_y = stx_tok _ expr _ comma_tok _ y_tok
+op_stx_absolute = stx_tok _ expr
+op_sty_zeropage = sty_tok _ expr
+op_sty_zeropage_x = sty_tok _ expr _ comma_tok _ x_tok
+op_sty_absolute = sty_tok _ expr
+op_tax_implied = _ tax_tok
+op_tay_implied = _ tay_tok
+op_tsx_implied = _ tsx_tok
+op_txa_implied = _ txa_tok
+op_txs_implied = _ txs_tok
+op_tya_implied = _ tya_tok
+
+
+oper = op_adc_immediate / op_adc_zeropage / op_adc_zeropage_x /
+op_adc_absolute / op_adc_absolute_x / op_adc_absolute_y /
+op_adc_indirect_x / op_adc_indirect_y / op_and_immediate / op_and_zeropage / op_and_zeropage_x / op_and_absolute / op_and_absolute_x / op_and_absolute_y / op_and_indirect_x / op_and_indirect_y / op_asl_accumulator / op_asl_zeropage / op_asl_zeropage_x / op_asl_absolute / op_asl_absolute_x / op_bcc_relative / op_bcs_relative / op_beq_relative / op_bit_zeropage / op_bit_absolute / op_bmi_relative / op_bne_relative / op_bpl_relative / op_brk_implied / op_bvc_relative / op_bvc_relative / op_clc_implied / op_cld_implied / op_cli_implied / op_clv_implied / op_cmp_immediate / op_cmp_zeropage / op_cmp_zeropage_x / op_cmp_absolute / op_cmp_absolute_x / op_cmp_absolute_y / op_cmp_indirect_x / op_cmp_indirect_y / op_cpx_immediate / op_cpx_zeropage / op_cpx_absolute / op_cpy_immediate / op_cpy_zeropage / op_cpy_absolute / op_dec_zeropage / op_dec_zeropage_x / op_dec_absolute / op_dec_absolute_x / op_dec_implied / op_dec_implied / op_eor_immediate / op_eor_zeropage / op_eor_zeropage_x / op_eor_absolute / op_eor_absolute_x / op_eor_absolute_y / op_eor_indirect_x / op_eor_indirect_y / op_inc_zeropage / op_inc_zeropage_x / op_inc_absolute / op_inc_absolute_x / op_inx_implied / op_iny_implied / op_jmp_absolute / op_jmp_indirect / op_jsr_absolute / op_lda_immediate / op_lda_zeropage / op_lda_zeropage_x / op_lda_absolute / op_lda_absolute_x / op_lda_absolute_y / op_lda_indirect_x / op_lda_indirect_y / op_ldx_immediate / op_ldx_zeropage / op_ldx_zeropage_y / op_ldx_absolute / op_ldx_absolute_y / op_ldy_immediate / op_ldy_zeropage / op_ldy_zeropage_x / op_ldy_absolute / op_ldy_absolute_x / op_lsr_accumulator / op_lsr_zeropage / op_lsr_zeropage_x / op_lsr_absolute / op_lsr_absolute_x / op_nop_implied / op_ora_immediate / op_ora_zeropage / op_ora_zeropage_x / op_ora_absolute / op_ora_absolute_x / op_ora_absolute_y / op_ora_indirect_x / op_ora_indirect_y / op_pha_implied / op_php_implied / op_pla_implied / op_plp_implied / op_rol_accumulator / op_rol_zeropage / op_rol_zeropage_x / op_rol_absolute / op_rol_absolute_x / op_ror_accumulator / op_ror_zeropage / op_ror_zeropage_x / op_ror_absolute / op_ror_absolute_x / op_rti_implied / op_rts_implied / op_sbc_immediate / op_sbc_zeropage / op_sbc_zeropage_x / op_sbc_absolute / op_sbc_absolute_x / op_sbc_absolute_y / op_sbc_indirect_x / op_sbc_indirect_y / op_sec_implied / op_sed_implied / op_sei_implied / op_sta_zeropage / op_sta_zeropage_x / op_sta_absolute / op_sta_absolute_x / op_sta_absolute_y / op_sta_indirect_x / op_sta_indirect_y / op_stx_zeropage / op_stx_zeropage_y / op_stx_absolute / op_sty_zeropage / op_sty_zeropage_x / op_sty_absolute / op_tax_implied / op_tay_implied / op_tsx_implied / op_txa_implied / op_txs_implied / op_tya_implied
+
+
 __ignored       = "comment" / ~r".*_tok" / "_"
 """
 
@@ -206,31 +418,16 @@ class Parser(ReduceParser):
     def _visit_one_arg(self, op, pos, arg):
         return Op(pos, op, arg)
 
-# flag for import-time initializaiton
 __setup_complete = False
 
-# amend the module by dynamically building the grammar and parser base class
+# amend the parser class by dynamically building visit methods
 if not __setup_complete:
-    op_names = []
-    grammar_parts = []
-    opname_tokens = []
-
-    for name in opcode_xref.keys():
-        grammar_parts.append(f'{name}_tok = "{name}"')
-
     for op in opcode_table:
         expr = f'op_{op.name}_{op.mode.name}'
-        seq = ' _ '.join([f'{op.name}_tok'] + addressmode_params[op.mode])
-        if len(addressmode_params[op.mode]) == 0:
-            seq = '_ ' + seq
-        op_names.append(expr)
-        grammar_parts.append(f'{expr} = {seq}')
         if addressmode_args[op.mode]:
             visit_name = '_visit_one_arg'
         else:
             visit_name = '_visit_no_args'
         fn = partialmethod(getattr(Parser, visit_name), op)
         setattr(Parser, f'visit_{expr}', fn)
-    grammar_parts.append('oper = ' + ' / '.join(op_names))
-    grammar += '\n'.join(grammar_parts)
 
